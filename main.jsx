4133e4f94d8208b997d7eecc4f21063b15d88d6c
malaikahub <aroojmalaika20@gmail.com>
2025-07-29 15:48:42 +0500

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -3,8 +3,5 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './App.css'   // ✅ Must be imported if styles are in App.css
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+
