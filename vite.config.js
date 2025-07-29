a586b3d8b2ea26aee2b253ab3752a428d1952660
malaikahub <aroojmalaika20@gmail.com>
2025-07-29 15:04:43 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Job-Application-portal/', // 👈 MUST match GitHub repo name exactly
   plugins: [react()],
+  base: '/Job-Application-portal/',  // ✅ EXACTLY this if your repo name is Job-Application-portal
 });
